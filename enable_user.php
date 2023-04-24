c3cf20bb30a3da32e23aaef0f57caffe6d3319a3
deanielledagondon <benben.bear143@gmail.com>
2023-04-24 21:37:15 +0800

    added enable/disable

---
+++ enable_user.php
@@ -0,0 +1,11 @@
+<?php
+include "config.php";
+
+if(isset($_GET['id'])) {
+    $user_id = $_GET['id'];
+    $sql = "UPDATE `users` SET `status` = 0 WHERE `id` = $user_id";
+    mysqli_query($conn, $sql);
+    header("Location: admin_users.php");
+    exit();
+}
+?>
